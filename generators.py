e4b8ae48f74c04311d31c869b46751020c5902b6
Bibar1s <zaksylykbibarys01@gmail.com>
2025-02-13 13:39:47 +0500

    upd

---
+++ generators.py
@@ -0,0 +1,23 @@
+# Task 1
+def square(n):
+  for i in range(n):
+    if i == 0:
+      continue
+    print(i**2)
+
+square(5)
+
+#Task 2
+def even(n):
+  for i in range(n+1):
+    if i % 2 == 0:
+      if i == n +1:
+        print(i)
+        countinue
+      print(i, end=',')
+
+n = int(input("Enter a number: "))
+even(n)
+
+#Task 3
+
